e90b4997b255c1cefa4c8bb50b7895725b73a591
DavidBarbosa425 <davidbarbosa425@gmail.com>
2021-07-13 20:51:03 -0300

    Estruturas condicionais if else

---
+++ Aula08/Ex013.js
@@ -0,0 +1,17 @@
+var agora = new Date()
+var hora = agora.getHours
+console.log (`São exatamente ${hora} horas`)
+
+if (hora >= 5 && hora<=11){
+    console.log('Bom dia')
+} 
+else if (hora >=12 && hora<=18){
+    console.log('Boa tarde')
+} 
+else if (hora >=0 && hora<5){
+    console.log('Boa madrugada')
+} 
+else{
+    console.log('Boa noite')
+}
+
