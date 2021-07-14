8e411a8aa1fbb95835b93f25c627d05db55eafae
DavidBarbosa425 <davidbarbosa425@gmail.com>
2021-07-13 23:37:47 -0300

    modelo

--- Aula09ex/Ex015/script.js
+++ Aula09ex/Ex015/script.js
@@ -1,1 +1,23 @@
-window.alert('Olá')
+
+function carregar(){
+
+var msg = window.document.getElementById('msg')
+var imagem = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `<strong>Agora são ${hora} horas</strong> `
+
+if (hora >=0 && hora <=11){
+    imagem.src = 'manha.png'
+    document.body.style.background = '#f6c897'
+} 
+else if(hora >=12 && hora <=18){
+    imagem.src = 'tarde.png'
+    document.body.style.background = '#dc5b01'
+}
+else {
+    imagem.src = 'noite.png'
+    document.body.style.background = '#151219'
+}
+
+}
