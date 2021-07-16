8178faecb1c76db8b44f31f35827411363587f72
DavidBarbosa425 <davidbarbosa425@gmail.com>
2021-07-15 21:53:22 -0300

    Gerador de Tabuada

--- Aula011ex/Ex018/script.js
+++ Aula011ex/Ex018/script.js
@@ -1,1 +1,22 @@
-window.alert('Olá')
+function clicar() {
+    var numero = window.document.querySelector('input#txtn')
+    var tab = window.document.querySelector('select#seltab')
+
+    var numT = Number(txtn.value)
+    
+    tab.innerHTML = ''
+    if (numT == 0){
+        window.alert('Digite um valor!')
+    } else{
+        for (c = 1; c <= 10; c++){
+            var r = (c * numT) 
+            
+            var item = document.createElement('option')
+            item.text = `${c} x ${numT} = ${r} `
+            tab.appendChild(item)
+        }
+    }
+
+    
+    
+}
